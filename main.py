c46de1c1e645a85af28b92da13ef58127705e12e
Kobby24 <geffah005@st.ug.edu.gh>
2025-01-16 16:09:24 +0000

    Groupings

--- main.py
+++ main.py
@@ -2,18 +2,28 @@ class Quine_McCluskey:
     def __init__(self):
         self.bin_terms = []
 
-
-    def Terms(self, min_terms: list, d_c: list,v_num):
+    def Terms(self, min_terms: list, d_c: list, v_num):
         for term in min_terms:
             bin_term = (bin(term)).split('b')[1]
-            if len(bin_term) <4:
-                add_ = 4-len(bin_term)
-                bin_term = ("0"*add_) + bin_term
+            if len(bin_term) < 4:
+                add_ = 4 - len(bin_term)
+                bin_term = ("0" * add_) + bin_term
             self.bin_terms.append(bin_term)
         return self.bin_terms
 
+    def Groups(self):
+        terms = self.Terms([0, 1, 3, 7, 8, 9, 11, 15], [9, 4], 4)
+        groups = {}
+        for term in terms:
+            try:
+                G = groups[f'G{term.count("1")}']
+
+            except KeyError:
+                groups[f'G{term.count("1")}'] = [term]
+            else:
+                G.append(term)
+        print(groups)
 
 
 MQ = Quine_McCluskey()
-print(MQ.Terms([0, 2, 3, 5], [9, 4],4))
-
+MQ.Groups()
